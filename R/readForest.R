15929a2dd9efdc27e1b1d9af21daf60a2350813e
Karl Kumbier <karl@MacBook-Pro.local>
2017-04-24 18:13:49 -0700

    debuggin readForest

--- R/readForest.R
+++ R/readForest.R
@@ -1,18 +1,16 @@
 readForest <- function(rfobj  # a randomForest object with forest component in it
-                       , X   # n x p data matrix 
+                       , x   # n x p data matrix 
                        , return_node_feature=TRUE
                        , return_node_data=TRUE
-                       , subsetFun = function(x) rep(TRUE, nrow(x))
-                       , wtFun = function(x) x$size_node
+                       , subsetFun = function(z) rep(TRUE, nrow(z))
+                       , wtFun = function(z) z$size_node
                        ){
   
   if (is.null(rfobj$forest))
     stop('No Forest component in the randomForest object')
-  if (!leaf_node_only)
-    stop('readForest not currently implemented for non-leaf nodes')
-
-  n <- nrow(X)
-  p <- ncol(X)
+  
+  n <- nrow(x)
+  p <- ncol(x)
   nrnodes <- rfobj$nrnodes
   ntree <- rfobj$ntree
 
@@ -24,7 +22,7 @@ readForest <- function(rfobj  # a randomForest object with forest component in i
                               row.names=NULL)
   out$tree_info$tree <- rep(1:rfobj$ntree, times=n.node.t)
   out$tree_info$parent <- unlist(parent)
-  parents <- out$tree_info$parent
+
 
   # Repeat each leaf node in node_feature based on specified sampling:
   # importance sampling = size_node
@@ -32,11 +30,12 @@ readForest <- function(rfobj  # a randomForest object with forest component in i
   rep.node <- rep(0, nrow(out$tree_info))
   select.node <- out$tree_info$status == -1
 
+  # Get observation counts for each leaf node
   tt <- matrix(0L, nrow=n, ncol=sum(select.node))
   obs.nodes <- apply(rfobj$obs.nodes, MAR=2, matchOrder) - 1
-  leaf.obs <- nodeObs(obs.nodes, n, ntree, table(out$tree_info$tree[leaf.node]), tt)
+  leaf.obs <- nodeObs(obs.nodes, n, ntree, table(out$tree_info$tree[select.node]), tt)
   out$tree_info$size_node <- 0
-  out$tree_info$size_node[leaf.node] <- colSums(leaf.obs)
+  out$tree_info$size_node[select.node] <- colSums(leaf.obs)
  
   select.node <- select.node & subsetFun(out$tree_info)
   out$tree_info <- out$tree_info[select.node,]
@@ -49,7 +48,7 @@ readForest <- function(rfobj  # a randomForest object with forest component in i
     sparse.idcs <- nodeVars(var.nodes, as.integer(ntree), 
                             as.integer(nrow(var.nodes)), 
                             as.integer(p),
-                            as.integer(parents),
+                            as.integer(parent),
                             as.integer(select.node),
                             as.integer(rep.node), 
                             as.integer(n.node.t),
