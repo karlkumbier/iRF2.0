741985f3113f42904799df05310b27990f6f0aea
wangyu <shifwangonline@gmail.com>
2018-01-07 09:47:58 -0800

    added unit test and fixed a bug in individual interactions

--- individual_paths_frequent_feature_interactions.R
+++ individual_paths_frequent_feature_interactions.R
@@ -1,15 +1,16 @@
-individual_paths_frequent_feature_interactions <- function(irf_output, sample_index, verbose = T){
+individual_paths_frequent_feature_interactions <- function(irf_output, data, sample_index, verbose = F){
   # Generate the frequent used feature interactions for an individual sample
   # Args:
-  #   irf_output: the output of irf package
-  #   sample_index: integer, which sample to look at
+  #   irf_output   : the output of irf package
+  #   data         : the input data, x
+  #   sample_index : integer, which sample to look at
   # Return:
   #   interactions: a list of frequent interactions for the individual
   require('iRF')
   require('arules')
   require('arulesViz')
   # read last forest
-  rf_last <- readForest(tail(irf_output$rf.list, n = 1)[[1]], x=x, return.node.obs=TRUE)
+  rf_last <- readForest(tail(irf_output$rf.list, n = 1)[[1]], x=data, return.node.obs=TRUE)
   # get the id of the target sample
   id <- rf_last$node.obs[,sample_index]
   # get the paths for the specific sample
